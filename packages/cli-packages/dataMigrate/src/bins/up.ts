65cdaf4bdf6bd626e2dd58ddf35867ded4f795df
Dominic Saadi <dominiceliassaadi@gmail.com>
2023-06-09 00:16:58 -0700

    add ts expect error just to get checks going

--- packages/cli-packages/dataMigrate/src/bins/up.ts
+++ packages/cli-packages/dataMigrate/src/bins/up.ts
@@ -5,5 +5,6 @@ import { command, description, builder } from '../commands/up'
 import { handler } from '../commands/upHandler'
 
 yargs(hideBin(process.argv))
+  // @ts-expect-error not sure why TS is complaining here, builder can be a function.
   .command(command, description, builder, handler)
   .parse()
